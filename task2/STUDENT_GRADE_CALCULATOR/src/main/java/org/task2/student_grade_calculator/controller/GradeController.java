099f5153a94d3c3c07032e7173b7fa65f839ce57
pegebas022 <pegebas@student.wethinkcode.co.za>
2023-12-20 10:40:40 +0200

    removed incomplete tasks

--- task2/STUDENT_GRADE_CALCULATOR/src/main/java/org/task2/student_grade_calculator/controller/GradeController.java
+++
@@ -1,8 +0,0 @@
-package org.task2.student_grade_calculator.controller;
-
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
-public class GradeController {
-
-}
--- task2/STUDENT_GRADE_CALCULATOR/src/main/java/org/task2/student_grade_calculator/dao/Subject.java
+++
@@ -1,4 +0,0 @@
-package org.task2.student_grade_calculator.dao;
-
-public class Subject {
-}
